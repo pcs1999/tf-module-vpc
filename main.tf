4d3aeac7e39b5314b7d87fee0b061fbc44d72a64
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-02-20 16:54:41 +0530

    tf-vpc_peering_auto_accept

--- main.tf
+++ main.tf
@@ -24,5 +24,6 @@ resource "aws_vpc_peering_connection" "peer" {
   peer_owner_id = data.aws_caller_identity.current.account_id
   peer_vpc_id =  "vpc-0738d5702c63820c7"
   vpc_id = aws_vpc.main.id
+  auto_accept = true
   
 }
