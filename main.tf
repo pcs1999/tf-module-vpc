4ae4244f755d5fb73449f375b7a8b0e0419b8f2b
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-02-20 17:31:20 +0530

    tf-vpc_peering_

--- main.tf
+++ main.tf
@@ -32,3 +32,9 @@ resource "aws_vpc_peering_connection" "peer" {
     )
   
 }
+
+resource "aws_route" "default" {
+  route_table_id = aws_vpc.main.default_route_table_id
+  destination_cidr_block = "172.31.0.0/16"
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
